93ac66d10253a70c3c2feb6fd60b3bdb392941bc
CEMS Developer <cems@example.com>
2026-05-03 22:32:34 +0800

    refactor: adopt Laravel 11 bootstrap pattern

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,60 +1,80 @@
 <?php
 
-use App\Exceptions\Handler;
-use App\Http\Kernel;
-use Illuminate\Contracts\Debug\ExceptionHandler;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\CheckRole;
+use App\Http\Middleware\CheckRoleAny;
+use App\Http\Middleware\DataBreachDetection;
+use App\Http\Middleware\EnsureMfaVerified;
+use App\Http\Middleware\IpBlocker;
+use App\Http\Middleware\PerformanceTrackingMiddleware;
+use App\Http\Middleware\QueryLogging;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\SecurityHeaders;
+use App\Http\Middleware\SessionTimeout;
+use App\Http\Middleware\StrictRateLimit;
+use App\Http\Middleware\ValidateSignature;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
+use Illuminate\Auth\Middleware\RequirePassword;
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
+use Illuminate\Http\Middleware\SetCacheHeaders;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api_v1.php',
+        commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Global middleware
+        $middleware->web(append: [
+            SecurityHeaders::class,
+            QueryLogging::class,
+            PerformanceTrackingMiddleware::class,
+        ]);
 
-$app = new Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class,
+        ]);
 
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
-*/
-
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-$app->singleton(
-    ExceptionHandler::class,
-    Handler::class
-);
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
-
-return $app;
+        // Middleware aliases
+        $middleware->alias([
+            'auth' => Authenticate::class,
+            'auth.basic' => AuthenticateWithBasicAuth::class,
+            'auth.session' => AuthenticateSession::class,
+            'cache.headers' => SetCacheHeaders::class,
+            'can' => Authorize::class,
+            'guest' => RedirectIfAuthenticated::class,
+            'password.confirm' => RequirePassword::class,
+            'precognitive' => HandlePrecognitiveRequests::class,
+            'signed' => ValidateSignature::class,
+            'throttle' => ThrottleRequests::class,
+            'verified' => EnsureEmailIsVerified::class,
+            'role' => CheckRole::class,
+            'role.any' => CheckRoleAny::class,
+            'mfa.verified' => EnsureMfaVerified::class,
+            'session.timeout' => SessionTimeout::class,
+            'security.headers' => SecurityHeaders::class,
+            'ip.blocker' => IpBlocker::class,
+            'throttle.login' => 'throttle:login',
+            'throttle.transactions' => 'throttle:transactions',
+            'throttle.str' => 'throttle:str-submission',
+            'throttle.bulk' => 'throttle:bulk',
+            'throttle.export' => 'throttle:export',
+            'throttle.sensitive' => 'throttle:sensitive',
+            'strict.ratelimit' => StrictRateLimit::class,
+            'data.breach' => DataBreachDetection::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
