20200c2f21dd137e9a13bbf006d0151a46598b39
CEMS Developer <cems@example.com>
2026-06-13 12:59:49 +0800

    chore: ignore root shell scripts and stop tracking

--- run-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# run-tests.sh - Run PHPUnit with opcache/xdebug disabled for stable execution
-# Usage: ./run-tests.sh [phpunit-args]
-
-php -d opcache.enable=0 -d xdebug.mode=off ./vendor/bin/phpunit "$@"
