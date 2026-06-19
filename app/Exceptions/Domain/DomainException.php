ee1292ee2d8e31f5c1871aeb4882a5311fb077c9
CEMS Developer <cems@example.com>
2026-06-20 03:18:48 +0800

    fix: add getStatusCode() to DomainException returning 422

--- app/Exceptions/Domain/DomainException.php
+++ app/Exceptions/Domain/DomainException.php
@@ -6,6 +6,11 @@ use RuntimeException;
 
 abstract class DomainException extends RuntimeException
 {
+    public function getStatusCode(): int
+    {
+        return 422;
+    }
+
     public function getSeverity(): string
     {
         return 'warning';
