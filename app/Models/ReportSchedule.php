723db27ec468cfb665029de95dfc79beba1ff3af
CEMS Developer <cems@example.com>
2026-06-22 02:09:39 +0800

    fix: add missing HasMany import in ReportSchedule

--- app/Models/ReportSchedule.php
+++ app/Models/ReportSchedule.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Cron\CronExpression;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ReportSchedule extends BaseModel
 {
