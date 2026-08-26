2fc281446d889f10e996b398d7af557a8c00d632
CEMS Developer <cems@example.com>
2026-08-27 06:37:23 +0800

    update(request): ResetPasswordRequest.php

--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
+use App\Rules\PasswordComplexityRule;
+use App\Rules\PasswordNotRecentlyUsed;
+
 class ResetPasswordRequest extends AuthorizedFormRequest
 {
     public function authorize(): bool
@@ -11,8 +15,17 @@ class ResetPasswordRequest extends AuthorizedFormRequest
 
     public function rules(): array
     {
+        /** @var User|null $user */
+        $user = $this->route('user');
+
         return [
-            'password' => 'required|string|min:12|confirmed|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+            'password' => [
+                'required',
+                'string',
+                'confirmed',
+                new PasswordComplexityRule,
+                new PasswordNotRecentlyUsed($user),
+            ],
         ];
     }
 }
