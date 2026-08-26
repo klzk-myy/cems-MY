9bd70a8ea09a2986e75b7f03125b58a0f69ca113
CEMS Developer <cems@example.com>
2026-08-27 06:37:57 +0800

    update(model): Branch.php

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -56,7 +56,7 @@ class Branch extends BaseModel
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<string>
+     * @var list<string>
      */
     protected $fillable = [
         'code',
