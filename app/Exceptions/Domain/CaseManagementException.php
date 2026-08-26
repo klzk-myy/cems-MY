ac2157ecadf57cb5f73dc47a80703c670d8c8865
CEMS Developer <cems@example.com>
2026-08-27 06:36:02 +0800

    update(exception): CaseManagementException.php

--- app/Exceptions/Domain/CaseManagementException.php
+++ app/Exceptions/Domain/CaseManagementException.php
@@ -4,9 +4,12 @@ namespace App\Exceptions\Domain;
 
 class CaseManagementException extends DomainException
 {
-    public function __construct(string $message)
-    {
-        parent::__construct($message);
+    public function __construct(
+        string $message,
+        int $code = 0,
+        ?\Throwable $previous = null,
+    ) {
+        parent::__construct($message, $code, $previous);
     }
 
     public function getStatusCode(): int
