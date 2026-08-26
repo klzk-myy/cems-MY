45ad105624aee30ead6de8c988dc27c5bdfc8ecb
CEMS Developer <cems@example.com>
2026-08-27 06:41:26 +0800

    update(factory): JournalLineFactory.php

--- database/factories/JournalLineFactory.php
+++ database/factories/JournalLineFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\ChartOfAccount;
 use App\Models\JournalEntry;
+use App\Models\JournalLine;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class JournalLineFactory extends Factory
 {
-    protected $model = \App\Models\JournalLine::class;
+    protected $model = JournalLine::class;
 
     public function definition(): array
     {
