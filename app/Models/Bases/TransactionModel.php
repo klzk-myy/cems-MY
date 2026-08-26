4c466a0ddfbf9a4a6beea88d52e6974719b2b854
CEMS Developer <cems@example.com>
2026-08-27 06:37:56 +0800

    update(model): TransactionModel.php

--- app/Models/Bases/TransactionModel.php
+++ app/Models/Bases/TransactionModel.php
@@ -9,7 +9,18 @@ use App\Models\Traits\BelongsToCustomer;
 use App\Models\Traits\BelongsToUser;
 use App\Models\Traits\HasApprover;
 use App\Models\Traits\HasStatus;
+use Illuminate\Support\Carbon;
 
+/**
+ * Base class for all transaction-like models sharing branch/currency/customer/user/approver relations.
+ *
+ * @property int|null $branch_id
+ * @property string $currency_code
+ * @property int $customer_id
+ * @property int $user_id
+ * @property int|null $approved_by
+ * @property Carbon|null $approved_at
+ */
 abstract class TransactionModel extends BaseModel
 {
     use BelongsToBranch,
