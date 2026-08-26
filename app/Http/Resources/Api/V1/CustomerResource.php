130d8409a170affd9c9790c6dbd239d9b9d5833a
CEMS Developer <cems@example.com>
2026-08-27 06:37:34 +0800

    update(resource): CustomerResource.php

--- app/Http/Resources/Api/V1/CustomerResource.php
+++ app/Http/Resources/Api/V1/CustomerResource.php
@@ -2,11 +2,55 @@
 
 namespace App\Http\Resources\Api\V1;
 
+use App\Enums\CddLevel;
+use App\Enums\IdType;
+use App\Enums\RiskRating;
+use App\Models\CustomerDocument;
+use App\Models\RiskScoreSnapshot;
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 /**
  * Transform a customer into a JSON resource.
+ *
+ * @property int $id
+ * @property string $full_name
+ * @property IdType $id_type
+ * @property-read string $id_number_masked
+ * @property string $nationality
+ * @property Carbon $date_of_birth
+ * @property string|null $address
+ * @property string|null $phone
+ * @property string|null $email
+ * @property bool $pep_status
+ * @property Carbon|null $pep_role_ended_at
+ * @property string|null $current_role_domain
+ * @property string|null $former_pep_domain
+ * @property bool $is_pep_associate
+ * @property bool $sanction_hit
+ * @property int $risk_score
+ * @property RiskRating $risk_rating
+ * @property CddLevel $cdd_level
+ * @property bool $is_active
+ * @property string|null $occupation
+ * @property string|null $employer_name
+ * @property string|null $employer_address
+ * @property float|null $annual_volume_estimate
+ * @property Carbon|null $risk_assessed_at
+ * @property Carbon|null $last_transaction_at
+ * @property bool $is_frozen
+ * @property string|null $freeze_reason
+ * @property Carbon|null $frozen_at
+ * @property bool $transactions_blocked
+ * @property string|null $rejection_reason
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, CustomerDocument>|null $documents
+ * @property-read Collection<int, Transaction>|null $transactions
+ * @property-read RiskScoreSnapshot|null $latestRiskSnapshot
  */
 class CustomerResource extends JsonResource
 {
