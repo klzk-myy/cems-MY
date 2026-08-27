274dc0bc68fc2ef6e73eba5f6be9572b09dece7f
CEMS Developer <cems@example.com>
2026-08-27 15:10:01 +0800

    fix(views): fix status-dot missing danger color key

--- resources/views/components/status-dot.blade.php
+++ resources/views/components/status-dot.blade.php
@@ -6,10 +6,12 @@
 $colors = [
     'success' => 'bg-success',
     'error' => 'bg-danger',
+    'danger' => 'bg-danger',
     'warning' => 'bg-warning',
     'info' => 'bg-info',
     'gray' => 'bg-ink-muted',
 ];
+$colorClass = $colors[$color] ?? 'bg-ink-muted';
 @endphp
 
-<span {{ ($attributes ?? new \Illuminate\View\ComponentAttributeBag)->merge(['class' => 'inline-block h-2 w-2 rounded-full ' . $colors[$color]]) }}></span>
+<span {{ ($attributes ?? new \Illuminate\View\ComponentAttributeBag)->merge(['class' => 'inline-block h-2 w-2 rounded-full ' . $colorClass]) }}></span>
