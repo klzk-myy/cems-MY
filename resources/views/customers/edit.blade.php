d8d79755d8391f48be3891e082521ca71ec4cf57
CEMS Developer <cems@example.com>
2026-09-11 21:51:37 +0800

    chore: commit changes for resources/views/customers/edit.blade.php

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -6,14 +6,17 @@
             @csrf
             @method('PUT')
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-                <x-input name="name" label="Full Name" :required="true" />
-                <x-input name="email" label="Email" type="email" :required="true" />
-                <x-input name="phone" label="Phone" />
-                <x-select name="id_type" label="ID Type" :options="['ic' => 'IC', 'passport' => 'Passport']" :required="true" />
-                <x-input name="id_number" label="ID Number" :required="true" />
+                <x-input name="name" label="Full Name" value="{{ $customer->full_name }}" :required="true" />
+                <x-input name="email" label="Email" type="email" value="{{ $customer->email }}" :required="true" />
+                <x-input name="phone" label="Phone" value="{{ $customer->phone }}" />
+                <x-select name="id_type" label="ID Type" :options="$idTypes" value="{{ $customer->id_type }}" :required="true" />
+                <x-input name="id_number" label="ID Number" value="{{ $decryptedIdNumber }}" :required="true" />
+                <x-select name="nationality" label="Nationality" :options="$nationalities" value="{{ $customer->nationality }}" />
+                <x-select name="risk_rating" label="Risk Rating" :options="$riskRatings" value="{{ $customer->risk_rating }}" />
+                <x-input name="birth_date" label="Date of Birth" type="date" value="{{ $customer->birth_date ? $customer->birth_date->toDateString() : '' }}" />
             </div>
-            <x-textarea name="address" label="Address" />
-            <x-textarea name="notes" label="Notes" />
+            <x-textarea name="address" label="Address" value="{{ $customer->address }}" />
+            <x-textarea name="notes" label="Notes" value="{{ $customer->notes }}" />
             <div class="flex justify-end gap-3">
                 <a href="{{ route('customers.index') }}"><x-button variant="secondary">Cancel</x-button></a>
                 <x-button type="submit" variant="primary">Update Customer</x-button>
