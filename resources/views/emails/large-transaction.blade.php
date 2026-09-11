689d4222f9c6f4444e7ff9072d50bec7620b098a
CEMS Developer <cems@example.com>
2026-09-11 21:51:39 +0800

    chore: commit changes for resources/views/emails/large-transaction.blade.php

--- resources/views/emails/large-transaction.blade.php
+++ resources/views/emails/large-transaction.blade.php
@@ -1,4 +1,4 @@
-<x-email-layout title="{{ "Large Transaction" }} Notification">
-    <p>This is an automated notification from CEMS regarding {{ str_replace('-', ' ', large-transaction) }}.</p>
+<x-email-layout title="Large Transaction Notification">
+    <p>This is an automated notification from CEMS regarding {{ str_replace('-', ' ', 'large-transaction') }}.</p>
     <p><a href="{{ url('/') }}" style="display: inline-block; padding: 12px 24px; background-color: #0a0a0a; color: #ffffff; text-decoration: none; border-radius: 6px; font-weight: 500;">View in CEMS</a></p>
 </x-email-layout>
