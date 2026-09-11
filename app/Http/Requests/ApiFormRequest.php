34316dcc32c4e85a8fd18db81d6c33df8107a912
CEMS Developer <cems@example.com>
2026-09-11 21:47:10 +0800

    chore: commit changes for app/Http/Requests/ApiFormRequest.php

--- app/Http/Requests/ApiFormRequest.php
+++ app/Http/Requests/ApiFormRequest.php
@@ -7,6 +7,25 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 
 abstract class ApiFormRequest extends AuthorizedFormRequest
 {
+    /**
+     * Form-request validation only inspects body/query by default. API
+     * routes that carry `{date}` in the path need it merged in so its
+     * `date_format` rule can run.
+     *
+     * @return array<string, mixed>
+     */
+    public function validationData(): array
+    {
+        $data = parent::validationData();
+
+        $routeDate = $this->route('date');
+        if ($routeDate !== null) {
+            $data['date'] = $routeDate;
+        }
+
+        return $data;
+    }
+
     protected function failedValidation(Validator $validator): void
     {
         throw new HttpResponseException(response()->json([
