3dea179e041d562539fffc62ae6a6d8d8e548349
CEMS Developer <cems@example.com>
2026-09-11 21:51:41 +0800

    chore: commit changes for resources/views/emails/sanctions-match.blade.php

--- resources/views/emails/sanctions-match.blade.php
+++ resources/views/emails/sanctions-match.blade.php
@@ -1,4 +1,4 @@
-<x-email-layout title="{{ "Sanctions Match" }} Notification">
-    <p>This is an automated notification from CEMS regarding {{ str_replace('-', ' ', sanctions-match) }}.</p>
+<x-email-layout title="Sanctions Match Notification">
+    <p>This is an automated notification from CEMS regarding {{ str_replace('-', ' ', 'sanctions-match') }}.</p>
     <p><a href="{{ url('/') }}" style="display: inline-block; padding: 12px 24px; background-color: #0a0a0a; color: #ffffff; text-decoration: none; border-radius: 6px; font-weight: 500;">View in CEMS</a></p>
 </x-email-layout>
