1bb31ea517b8aee954de2c1c9d5354192f1e6963
CEMS Developer <cems@example.com>
2026-09-11 21:51:36 +0800

    chore: commit changes for resources/views/counters/open.blade.php

--- resources/views/counters/open.blade.php
+++ resources/views/counters/open.blade.php
@@ -1,6 +1,6 @@
-<x-app-layout title="{{ "Open" }} Counter">
-    <x-page-header title="{{ "Open" }} Counter" description="Counter management" />
+<x-app-layout title="Open Counter">
+    <x-page-header title="Open Counter" description="Counter management" />
     <x-card>
-        <p class="text-ink-muted">Counter {{ open }} view content.</p>
+        <p class="text-ink-muted">Counter view content.</p>
     </x-card>
 </x-app-layout>
